304efc663c1b62a3bab512c2e24c80e7aa092255
Mohamed Mostafa <mohamedmostafa97628@gmail.com>
2019-11-02 03:00:26 +0200

    fix notif style

--- resources/views/site/layouts.blade.php
+++ resources/views/site/layouts.blade.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">        
         <link rel="stylesheet" href="{{ asset('dest/css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('dest/css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('PNotifyBrightTheme.css') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
     </head>
     <body>
